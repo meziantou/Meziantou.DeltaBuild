3fd71413c2c00f64daf2f5a17bf1ee305be55564
Gérald Barré <git@meziantou.net>
2026-03-26 00:28:25 -0400

    Cleanup

--- tests/Meziantou.DeltaBuild.Tests/ModuleInitializer.cs
+++ tests/Meziantou.DeltaBuild.Tests/ModuleInitializer.cs
@@ -6,6 +6,7 @@ namespace Meziantou.DeltaBuild.Tests;
 internal static class ModuleInitializer
 {
     [ModuleInitializer]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2255:The 'ModuleInitializer' attribute should not be used in libraries")]
     internal static void Initialize()
     {
         InlineSnapshotSettings.Default = InlineSnapshotSettings.Default with
